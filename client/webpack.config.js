f7b82adc6dcaeece56a66ce803e5177ebab2cfc3
Nardgelen Jean Francois <nardgelenjf@gmail.com>
2024-06-08 21:59:10 -0400

    additional dependencies

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+// const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
